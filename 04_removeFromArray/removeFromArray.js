74634c7ca65e0bbd547e2af215fc55bb4d27ad1f
Victor Hugo Damo Pimentel <victor_hugo.d.pimentel@hotmail.com>
2022-04-14 04:14:50 -0300

    Add partial solution to removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, removalTarget) {
+    const outputArray = inputArray;
+    let matchFound = false;
+    let i = 0;
+    while (!matchFound) {
+        const currentElement = inputArray[i];
+        matchFound = (removalTarget === currentElement);
+        
+        if (matchFound) {
+            outputArray.splice(i, 1);
+        }
 
+        i++;
+    }
+    return outputArray;
 };
 
 // Do not edit below this line
