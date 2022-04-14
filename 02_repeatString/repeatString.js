73e0fbe4ef7999da7db15c028daf9b1984e55e4c
Victor Hugo Damo Pimentel <victor_hugo.d.pimentel@hotmail.com>
2022-04-13 23:12:40 -0300

    Add my solution to repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(inputString, n) {
+    let outputString = '';
+    
+    if (n < 0) return 'ERROR';
 
+    for (i = 0; i < n; i++) {
+        outputString += inputString;
+    }
+    
+    return outputString;
 };
 
 // Do not edit below this line
