44d7360076e59fc2363ae70142ceda413c8e1286
Victor Hugo Damo Pimentel <victor_hugo.d.pimentel@hotmail.com>
2022-04-16 22:56:38 -0300

    Removed useless variables on removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,13 +2,12 @@ const removeFromArray = function(inputArray, ...removalTargets) {
 
     for (let i = 0; i < removalTargets.length; i++) {
 
-        let currentTarget = removalTargets[i];    
-        
         let matchFound = false;
         let j = 0;
+
         while (!matchFound && j < inputArray.length) {
-            const currentElement = inputArray[j];
-            matchFound = (currentTarget === currentElement);
+
+            matchFound = (removalTargets[i] === inputArray[j]);
             
             if (matchFound) {
                 inputArray.splice(j, 1);
