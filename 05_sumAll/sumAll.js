89f8bf00b3217062ae09d163bf86c17d60fb86ce
Victor Hugo Damo Pimentel <victor_hugo.d.pimentel@hotmail.com>
2022-04-18 01:13:57 -0300

    Add my solution to sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(integerOne, integerTwo) {
+    if(integerOne < 0 || integerTwo < 0 || typeof(integerOne) !== 'number' ||
+        typeof(integerTwo) !== 'number') return 'ERROR';
 
+    const a = (integerOne < integerTwo)? integerOne : integerTwo;
+    const b = (integerTwo > integerOne)? integerTwo : integerOne;
+
+    const result = ((a + b) / 2) * (1 + b - a); 
+    return result;
 };
 
 // Do not edit below this line
