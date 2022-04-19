be8d03ae4c6fdbf6130a8df8dd0e3c7b8079738d
Victor Hugo Damo Pimentel <victor_hugo.d.pimentel@hotmail.com>
2022-04-19 07:07:54 -0300

    Add my solution to tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(fahrenheitTemp) { 
+  //celsius = (fahrenheit - 32) * 5/9
+  const celsiusTemp = (fahrenheitTemp - 32) * 5/9;
 
+  return Number(celsiusTemp.toFixed(1));
 };
 
-const ctof = function() {
+const ctof = function(celsiusTemp) {
+  //fahrenheit = celsius * 9/5 + 32
+  const fahrenheitTemp = (celsiusTemp * 9 / 5) + 32;
 
+  return Number(fahrenheitTemp.toFixed(1));
 };
 
 // Do not edit below this line
