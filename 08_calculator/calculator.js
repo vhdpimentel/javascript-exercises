516c1211dfae6b01ad841b469721171e2c499ee2
Victor Hugo Damo Pimentel <victor_hugo.d.pimentel@hotmail.com>
2022-04-19 07:32:36 -0300

    Add my solution to calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(multiplicands) {
+  let result = multiplicands[0];
+  for (let i = 1; i < multiplicands.length; i++) {
+    result *= multiplicands[i];
+  }
 
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let result = 1;
+  let multiplier = x;
+  for (let i = x; i > 0; i--) {
+    result *= multiplier;
+    multiplier--;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
