6a0ce9aae08c3ff748e7cf124fd764413d438560
Victor Hugo Damo Pimentel <victor_hugo.d.pimentel@hotmail.com>
2022-04-19 09:10:17 -0300

    Add my solution to fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
 
+    if (n === 1 || n === 2) {
+        return 1;
+    } else if (n > 1){
+        return fibonacci(n - 1) + fibonacci(n - 2); 
+    } else return 'OOPS';                                          
 };
 
 // Do not edit below this line
