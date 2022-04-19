c105ab9776df6ef2651f8efdd7e8695c16c3dba4
Victor Hugo Damo Pimentel <victor_hugo.d.pimentel@hotmail.com>
2022-04-19 08:57:30 -0300

    Add my solution to palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let positive = true;
 
+    //filter out punctuation, upper case and spaces
+    const filteredWord = word.replace(/[^a-zA-Z]+/g, '').toLowerCase();
+
+    let i = 0;
+    while (positive && i < filteredWord.length / 2){
+        positive = (filteredWord[i] === filteredWord[filteredWord.length - i - 1]);
+        i++;
+    }
+    return positive;
 };
 
 // Do not edit below this line
